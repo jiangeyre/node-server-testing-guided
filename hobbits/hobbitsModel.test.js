289f03ea6f81b1f1f47e7e7587204381ae22dd31
Lexie Jiang <LexieJiang@Lexies-MacBook-Pro.local>
2020-01-30 11:16:36 -0800

    Commit 5

--- hobbits/hobbitsModel.test.js
+++ hobbits/hobbitsModel.test.js
@@ -1,48 +1,64 @@
-const Hobbits = require('./hobbitsModel');
+const Hobbits = require('./hobbitsModel.js');
+const db = require('../data/dbConfig.js');
 
-const db = require('../data/dbConfig');
-
-describe('test environment', function() {
-
-    it('should use the testing environment', function() {
-        expect(process.env.DB_ENV).toBe('testing');
+describe('hobbits model', function() {
+    
+    
+    describe('test environment', function() {
+        it('should use the testing environment', function() {
+            expect(process.env.DB_ENV).toBe('testing');
+        })
     })
-
+    
     describe('insert()', function() {
         beforeEach(async () => {
-            // this function executes and clears out the table before each test
             await db('hobbits').truncate();
         })
-    })
 
-    describe.skip('insert', function() {
-        it('adds new hobbit to db', async function() {
+        it('adds the new hobbit to the db', async function() {
             // call insert passing a hobbit
             await Hobbits.insert({ name: 'sam' });
             await Hobbits.insert({ name: 'frodo' });
-            //open the db and see that the hobbit is there
+
+            // open the db and see that the hobbit is there
             const hobbits = await db('hobbits');
 
             expect(hobbits).toHaveLength(2);
         })
-    })
+        
+        it('adds the new hobbit to the db', async function() {
+            // call insert passing a hobbit
+            await Hobbits.insert({ name: 'sam' });
+            await Hobbits.insert({ name: 'frodo' });
 
+            // open the db and see that the hobbit is there
+            const hobbits = await db('hobbits');
+
+            expect(hobbits).toHaveLength(2);
+        })
+
+
+    })
     describe('delete()', function() {
         beforeEach(async () => {
-            // this function executes and clears out the table before each test
             await db('hobbits').truncate();
         })
-    })
 
-    describe.skip('delete', function() {
-        it('removes hobbit from db', async function() {
-            // call insert passing a hobbit
-            await Hobbits.insert({ name: 'sam' });
-            await Hobbits.insert({ name: 'frodo' });
-            //open the db and see that the hobbit is there
-            const hobbits = await db('hobbits');
+        it('removes the hobbit from the db', async function() {
+            // check that the table is empty
 
-            expect(hobbits).toHaveLength(2);
+            // add a hobbit
+
+            // check that the hobbit is there
+
+            // delete the hobbit
+
+            // check that the hobbit is gone 
+        
         })
+
+
     })
+    
 })
+
